d2167dd1918044b77d14b8f7461994e472507676
PIXEL <PIXEL@PIXEL-PC>
2018-10-01 15:18:56 +0530

    First stagged

---
+++ LoginPage/Registration.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Registration : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        this.UnobtrusiveValidationMode = System.Web.UI.UnobtrusiveValidationMode.None;
+        zdgffscsdfxgxv
+            xc,mvnxcmvn
+             vzxn vdzc
+    }
+
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        Response.Write("Registration Successful");
+    }
+
+    protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+    {
+
+    }
+}
